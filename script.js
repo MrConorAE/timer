961a627a5b0b29f294688b8dc32c208b8ece105a
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2019-11-26 19:02:10 +1300

    Dark theme and fonts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var dateNow;
 var dateSoon;
 var dateDiff;
+var countValues = [];
 
 function init() {
     dateSoon = new Date();
@@ -8,11 +9,34 @@ function init() {
         dateSoon.setFullYear((prompt("Year:")), (prompt("Month:")), (prompt("Day:")));
     }
     dateSoon.setHours((prompt("Hour:")-12), (prompt("Minute:")));
+    dateSoon.setSeconds(0);
     setInterval(function () {
         dateNow = new Date();
         document.getElementById('time').innerHTML = dateNow.toLocaleTimeString();
         dateDiff = new Date(dateSoon - dateNow);
-        document.getElementById('count').innerHTML = (dateDiff.getHours() + "h " + dateDiff.getMinutes() + "m " + dateDiff.getSeconds() + "s " + dateDiff.getMilliseconds() + "ms");
+        if (String(dateDiff.getHours()).length == 1) {
+            countValues[0] = "0" + dateDiff.getHours();
+        } else {
+            countValues[0] = dateDiff.getHours();
+        }
+        if (String(dateDiff.getMinutes()).length == 1) {
+            countValues[1] = "0" + dateDiff.getMinutes();
+        } else {
+            countValues[1] = dateDiff.getMinutes();
+        }
+        if (String(dateDiff.getSeconds()).length == 1) {
+            countValues[2] = "0" + dateDiff.getSeconds();
+        } else {
+            countValues[2] = dateDiff.getSeconds();
+        }
+        if (String(dateDiff.getMilliseconds()).length == 2) {
+            countValues[3] = "0" + dateDiff.getMilliseconds();
+        } else if (String(dateDiff.getMilliseconds()).length == 1) {
+            countValues[3] = "00" + dateDiff.getMilliseconds();
+        } else {
+            countValues[3] = dateDiff.getMilliseconds();
+        }
+        document.getElementById('count').innerHTML = countValues[0] + ":" + countValues[1] + ":" + countValues[2] + "." + countValues[3];
     }, 10);
 }
 
